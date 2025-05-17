c2634a24ce1d838bc57831717281687987e66432
Damon Myers <damon.shane.myers@gmail.com>
2025-05-17 00:59:30 -0700

    fix: build errors

--- README.md
+++ README.md
@@ -1,14 +1,24 @@
 # pnwcubedrafts
 
 A static website built with Next.js for a community of [Cube Drafters](https://mtg.fandom.com/wiki/Cube_Draft)
-in the PNW of the United States.
+located in the Pacific Northwest of the United States.
 
 ## Getting Started
 
-First, run the development server:
+Run the development server:
 
 ```bash
 npm run dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+## Build and deploy
+
+This site is hosted with [GitHub Pages](https://pages.github.com/).
+
+To make a production build:
+
+```bash
+npm run build
+```
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,10 +23,10 @@ export default function Home() {
         <p className="text-lg mb-4">
           <a href="https://luckypaper.co/what-is-a-cube/" target="_blank" className="text-blue-300 hover:underline">Cube</a> is a way to play Magic: the Gathering in which players build decks from a curated set of cards known as a cube.
           A cube might be themed around a specific format, set, mechanic, or even <a href="https://cubecobra.com/cube/overview/100-ornithopters" target="_blank" className="text-blue-300 hover:underline">a single card</a>.
-          This makes Cube the ideal way to tailor your game to the aspects of the Magic that you enjoy the most, while avoiding those that you don't.
+          This makes Cube the ideal way to tailor your game to the aspects of the Magic that you enjoy the most, while avoiding those that you don&pos;t.
         </p>
         <p className="text-lg mb-4">
-          Because the cards are provided by the designer of the cube, players don't need to purchase <i>anything</i> to play.
+          Because the cards are provided by the designer of the cube, players don&pos;t need to purchase <i>anything</i> to play.
           Cube is an excellent way for newcomers to enjoy the game without having to invest huge sums of money into building and maintaining a collection.
         </p>
       </div>
@@ -39,7 +39,7 @@ export default function Home() {
         </p>
         <ul className="list-disc px-8">
           <li className="text-lg mb-4">
-            <a href="https://discord.gg/h45vZ3Hr8x" target="_blank" className="text-blue-300 hover:underline">PNW Cube Drafters Discord</a> - Our community's Discord server. We'll happily answer any questions you have!
+            <a href="https://discord.gg/h45vZ3Hr8x" target="_blank" className="text-blue-300 hover:underline">PNW Cube Drafters Discord</a> - Our community&pos;s Discord server. We&pos;ll happily answer any questions you have!
           </li>
           <li className="text-lg mb-4">
             <a href="https://luckypaper.co/podcast/" target="_blank" className="text-blue-300 hover:underline">Lucky Paper Radio</a> - A podcast made by cubers in Baltimore, MD. Their website is a treasure trove of resources on Cube design and play.
