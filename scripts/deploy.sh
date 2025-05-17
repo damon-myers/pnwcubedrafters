b6a6f5a4b496113f702f169a69fd30c5fb929505
Damon Myers <damon.shane.myers@gmail.com>
2025-05-17 01:50:39 -0700

    fix: deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,4 +9,4 @@ echo 'pnwcubedrafters.com' > ./out/CNAME
 # allow gh pages to scan directories that start with _ (aka _next/)
 echo 'true' > ./out/.nojekyll
 
-gh-pages --src out -b main -r https://github.com/tyrelh/tyrelh.github.io.git -f
+gh-pages -d out -b gh-pages -f
